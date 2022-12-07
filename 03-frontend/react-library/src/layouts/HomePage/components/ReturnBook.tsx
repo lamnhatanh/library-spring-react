581a249e8f9e86312659d9509dcd06ac19e05686
lamnhatanh <anhlam051595@gmail.com>
2022-12-06 23:59:23 -0500

    fetch data to carousel

--- 03-frontend/react-library/src/layouts/HomePage/components/ReturnBook.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/components/ReturnBook.tsx
@@ -1,15 +1,28 @@
-export const ReturnBook = () => {
+import React from "react";
+import BookModel from "../../../models/BookModel";
+
+export const ReturnBook: React.FC<{ book: BookModel }> = (props) => {
     return (
         <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
             <div className="text-center">
-                <img
-                    src={require("./../../../Images/BooksImages/crash-python.png")}
-                    width="151"
-                    height="233"
-                    alt="book"
-                />
-                <h6 className="mt-2">Book</h6>
-                <p>Hello</p>
+                {props.book.img ? (
+                    <img
+                        src={props.book.img}
+                        width="151"
+                        height="233"
+                        alt="book"
+                    />
+                ) : (
+                    <img
+                        src={require("./../../../Images/BooksImages/crash-python.png")}
+                        width="151"
+                        height="233"
+                        alt="book"
+                    />
+                )}
+
+                <h6 className="mt-2">{props.book.title}</h6>
+                <p>{props.book.author}</p>
                 <a className="btn main-color text-white" href="#">
                     Reserve
                 </a>
